1205d167baa10088a23e4a1b87c45e5d4a066d19
Agung Budie Pangestu <agungbudiepangestu@gmail.com>
2024-05-13 12:38:54 +0700

    create forgot password page

--- components/auth/new-password-form.tsx
+++ components/auth/new-password-form.tsx
@@ -2,7 +2,7 @@
 
 import * as z from "zod"
 import { CardWrapper } from "./card-wrapper"
-import { LoginSchema } from "@/schemas"
+import { NewPasswordSchema } from "@/schemas"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Input } from "../ui/input"
@@ -10,33 +10,31 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Button } from "../ui/button"
 import { FormError } from "../form-error"
 import { FormSuccess } from "../form-success"
-import { login } from "@/actions/login"
+import { newPassword } from "@/actions/new-password"
 import { useState, useTransition } from "react"
 import { useSearchParams } from "next/navigation"
-import Link from "next/link"
 
-export const LoginForm = () => {
+export const NewPasswordForm = () => {
     const searchParams = useSearchParams()
-    const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ? "Email already in use with different provider" : ""
+    const token = searchParams.get("token")
 
     const [error, setError] = useState<string | undefined>("")
     const [success, setSuccess] = useState<string | undefined>("")
     const [isPending, startTransition] = useTransition()
 
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof NewPasswordSchema>>({
+        resolver: zodResolver(NewPasswordSchema),
         defaultValues: {
-            email: "",
-            password: ""
+            password: "",
         }
     })
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
         setError("")
         setSuccess("")
 
         startTransition(() => {
-            login(values)
+            newPassword(values, token)
                 .then((data) => {
                     setError(data?.error)
                     setSuccess(data?.success)
@@ -46,10 +44,9 @@ export const LoginForm = () => {
 
     return (
         <CardWrapper
-            headerLabel="Welcome back"
-            backButtonLabel="Don't have an account?"
-            backButtonHref="/auth/register"
-            showSocial
+            headerLabel="Enter a new password"
+            backButtonLabel="Back to Login"
+            backButtonHref="/auth/login"
         >
             <Form {...form}>
                 <form
@@ -57,24 +54,6 @@ export const LoginForm = () => {
                     className="space-y-6"
                 >
                     <div className="space-y-4">
-                        <FormField
-                            control={form.control}
-                            name="email"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            disabled={isPending}
-                                            type="email"
-                                            placeholder="Enter your email"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
                         <FormField
                             control={form.control}
                             name="password"
@@ -86,30 +65,22 @@ export const LoginForm = () => {
                                             {...field}
                                             disabled={isPending}
                                             type="password"
-                                            placeholder="Enter your password"
+                                            placeholder="Enter your new password"
                                         />
                                     </FormControl>
-                                    <Button
-                                        size="sm"
-                                        variant="link"
-                                        asChild
-                                        className="px-0 font-normal"
-                                    >
-                                        <Link href="/auth/reset">Forgot password?</Link>
-                                    </Button>
                                     <FormMessage />
                                 </FormItem>
                             )}
                         />
                     </div>
-                    <FormError message={error || urlError} />
+                    <FormError message={error} />
                     <FormSuccess message={success} />
                     <Button
                         disabled={isPending}
                         type="submit"
                         className="w-full"
                     >
-                        Login
+                        Reset password
                     </Button>
                 </form>
             </Form>
